9ceb03160707ffbaea422b93f489b6c1c53a1231
Bryan Yeo <bryandismas@simiansoftwerks.com>
2025-12-26 17:00:55 -0800

    plugin prints notification!

--- lua/foundry/init.lua
+++ lua/foundry/init.lua
@@ -2,6 +2,10 @@ local M = {}
 
 function M.setup(opts)
 	M.opts = opts or {}
+
+	vim.api.nvim_create_user_command('FoundryHello', function()
+		vim.notify('Hello from foundry.nvim!')
+	end, {})
 end
 
 return M
