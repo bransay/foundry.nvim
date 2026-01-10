950966a940a92af4c50adebc7871758dfd33ee78
Bryan Yeo <bryeo@YeoDesktop.lan>
2026-01-09 17:12:41 -0800

    complete execute

--- lua/foundry/exec.lua
+++ lua/foundry/exec.lua
@@ -8,10 +8,13 @@ local ExecutablePriority = {
 	BELOW_NORMAL = 1
 }
 
+-- Cross platform priority control - important when compiling because not 
+-- lowering the priority of compilation can cause UI unresponsiveness, etc.
 -- @param prog_name string
 -- @param args string[]
 -- @param priority ExecutablePriority
-function M.exec(prog_name, args, priority)
+-- @param output_callback fun(result:string):nil
+function M.exec(prog_name, args, priority, output_callback)
 	if not prog_name then
 		return nil, 'Requires a program name'
 	end
@@ -22,6 +25,17 @@ function M.exec(prog_name, args, priority)
 
 	args = args or {}
 	priority = priority or ExecutablePriority.NORMAL
+
+	-- TODO: actually do something with the priority being passed in
+
+	table.insert(args, 1, prog_name)
+	vim.system(
+		args,
+		{ text = true },
+		function(res)
+			output_callback(res.stdout)
+		end
+	)
 end
 
 return M
