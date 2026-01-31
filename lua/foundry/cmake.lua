53d018c6664404040cef22992358f6ddaad869a4
Bryan Yeo <bryandismas@simiansoftwerks.com>
2026-01-31 14:37:10 -0800

    choosing targets works now!

--- lua/foundry/cmake.lua
+++ lua/foundry/cmake.lua
@@ -27,19 +27,20 @@ end
 local get_targets_func = nil
 local function get_targets()
 	if not get_targets_func then
-		return {}
+		return nil
 	end
 	return get_targets_func()
 end
 
 local options = {
-	PRESET = { 'preset', 'Preset', foundry_options.select_picker(get_preset_options) },
 	BUILD_DIR = { 'build_dir', 'Build Directory', foundry_options.directory_picker() },
+	PRESET = { 'preset', 'Preset', foundry_options.select_picker(get_preset_options) },
 	TARGET = { 'target', 'Target', foundry_options.select_picker(get_targets) }
 }
 
-local function get_option(option, show_ui)
+local function get_option(option, default, show_ui)
 	show_ui = show_ui or false
+	---@type OptionOpts
 	local opts = {}
 	if show_ui then
 		opts.force_ui = true
@@ -47,7 +48,14 @@ local function get_option(option, show_ui)
 	if option[3] then
 		opts.picker = option[3]
 	end
+	if default then
+		opts.default = default
+	end
 	return foundry_options.get(option[1], option[2], opts)
+end
+
+local function get_default_build_dir(preset)
+	return vim.fs.joinpath(vim.fn.getcwd(), 'build', preset)
 end
 
 get_targets_func = function()
@@ -57,16 +65,75 @@ get_targets_func = function()
 		return nil
 	end
 
-	local build_dir = get_option(options.BUILD_DIR)
-	if not build_dir then
-		vim.notify('Choosing targets requires build directory', vim.log.levels.ERROR)
+	local build_dir = get_option(options.BUILD_DIR, get_default_build_dir(preset))
+	assert(build_dir, 'build_dir must be valid')
+
+	-- use the cmake file api to find all targets
+	local api_dir = vim.fs.joinpath(build_dir, '.cmake', 'api', 'v1')
+	local query_dir = vim.fs.joinpath(api_dir, 'query', 'client-foundry.nvim')
+	vim.fn.mkdir(query_dir, '-p')
+	local query_file = vim.fs.joinpath(query_dir, 'codemodel-v2')
+	if vim.fn.writefile({}, query_file) ~= 0 then
 		return nil
 	end
 
-	-- TODO: return targets
-	return {
-		"TODO"
-	}
+	M.generate()
+
+	-- look for the reply
+	local codemodel_file = vim.fs.joinpath(api_dir, 'reply', 'codemodel-v2-*.json')
+	codemodel_file = vim.fn.glob(codemodel_file, true, true)
+
+	if #codemodel_file == 0 then
+		vim.notify('Cmake file api failure - no response', vim.log.levels.ERROR)
+		return nil
+	end
+	codemodel_file = codemodel_file[1]
+
+	if not vim.fn.filereadable(codemodel_file) then
+		vim.notify('Cmake file api failure - could not read response', vim.log.levels.ERROR)
+		return nil
+	end
+
+	local contents = vim.fn.readfile(codemodel_file)
+	contents = table.concat(contents, '\n')
+
+	local function invalid_response()
+		vim.notify('Cmake file api failure - invalid response', vim.log.levels.ERROR)
+		return nil
+	end
+
+	local codemodel = vim.json.decode(contents)
+	if not codemodel then
+		return invalid_response()
+	end
+
+	local configurations = codemodel.configurations
+	if not configurations then
+		return invalid_response()
+	end
+
+	local targets = {}
+	for _, configuration in ipairs(configurations) do
+		if not configuration then
+			return invalid_response()
+		end
+
+		local configuration_targets = configuration.targets
+		if not configuration_targets then
+			return invalid_response()
+		end
+
+		for _, target in ipairs(configuration_targets) do
+			local target_name = target.name
+			if not target_name then
+				return invalid_response()
+			end
+
+			table.insert(targets, target_name)
+		end
+	end
+
+	return targets
 end
 
 function M.generate()
@@ -77,12 +144,15 @@ function M.generate()
 		return
 	end
 
+	local build_dir = get_option(options.BUILD_DIR, get_default_build_dir(preset))
+	assert(build_dir, 'build_dir must be valid')
+
 	-- TODO: let's do persistent notifications that last until the preset is complete
 	vim.notify('Generating preset: ' .. preset .. '...')
 
 	-- kick off generating task
 	local task_name = 'Generating preset: ' .. preset
-	local result = setup_opts.task(task_name, { 'cmake', '--preset', preset })
+	local result = setup_opts.task(task_name, { 'cmake', '--preset', preset, '-B', build_dir })
 
 	if result then
 		vim.notify(task_name .. ' succeeded')
@@ -99,12 +169,17 @@ function M.build()
 		return
 	end
 
+	local build_dir = get_option(options.BUILD_DIR, get_default_build_dir(preset))
+	assert(build_dir, 'build_dir must be valid')
+
+	-- TODO: we should be building a target, not just everything.. right? not sure
+
 	-- TODO: let's do persistent notifications that last until the preset is complete
 	vim.notify('Building preset: ' .. preset .. '...')
 
 	-- kick off generating task
 	local task_name = 'Building preset: ' .. preset
-	local result = setup_opts.task(task_name, { 'cmake', '--build', '--preset', preset })
+	local result = setup_opts.task(task_name, { 'cmake', '--build', '--preset', preset, '-B', build_dir })
 
 	if result then
 		vim.notify(task_name .. ' succeeded')
@@ -151,7 +226,7 @@ function M.options()
 	end
 	choice = menu_options[choice]
 
-	get_option(choice, true)
+	get_option(choice, nil, true)
 end
 
 function M.actions()
