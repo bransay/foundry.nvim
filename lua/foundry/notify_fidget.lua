0e15043e1d28ba42896f0cacba371aee21e3467a
Bryan Yeo <bryandismas@simiansoftwerks.com>
2026-03-01 12:12:00 -0800

    implement fidget notifications

--- lua/foundry/notify_fidget.lua
+++ lua/foundry/notify_fidget.lua
@@ -5,6 +5,7 @@ local fidget_notify = require("fidget.notification")
 local M = {}
 
 local next_id = 0
+local active_notifications = {}
 
 --- @param msg string
 --- @param opts? NotifyOpts
@@ -14,23 +15,48 @@ function M.notify(msg, opts)
 	local level = opts.level or vim.log.levels.INFO
 
 	next_id = next_id + 1
+	local id = next_id
+
+	active_notifications[id] = level
 
 	local fidget_opts = {
 		annote = NOTIFICATION_TITLE,
-		ttl = opts.keep and math.huge or nil,
-		key = next_id,
+		key = id,
 	}
+
+	if opts.keep then
+		fidget_opts.ttl = math.huge
+	end
 
 	fidget_notify.notify(msg, level, fidget_opts)
 
-	return next_id
+	return id
 end
 
 --- @param id integer
 --- @param msg string
-function M.update(id, msg) end
+function M.update(id, msg)
+	local level = active_notifications[id] or vim.log.levels.INFO
+
+	local fidget_opts = {
+		annote = NOTIFICATION_TITLE,
+		key = id,
+		update_only = true,
+	}
+
+	fidget_notify.notify(msg, level, fidget_opts)
+end
 
 --- @param id integer
-function M.dismiss(id) end
+function M.dismiss(id)
+	active_notifications[id] = nil
+
+	local fidget_opts = {
+		key = id,
+		update_only = true,
+	}
+
+	fidget_notify.notify(nil, vim.log.levels.INFO, fidget_opts)
+end
 
 return M
