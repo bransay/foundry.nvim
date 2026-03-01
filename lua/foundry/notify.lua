a6c804dc096d55ba6aaa138edd7dbf895f959d11
Bryan Yeo <bryandismas@simiansoftwerks.com>
2026-03-01 11:46:16 -0800

    okay interface refined

--- lua/foundry/notify.lua
+++ lua/foundry/notify.lua
@@ -1,14 +1,25 @@
+local NOTIFICATION_TITLE = "foundry"
+
 local M = {}
 
-function M.setup(opts)
-	M.opts = opts or {}
-end
+--- @class NotifyOpts
+--- @field level? number vim.log.levels.INFO, WARN, ERROR, etc. Defaults to INFO
+--- @field keep? boolean Keep notification persistent (don't auto-dismiss)
 
-function M.notify(msg, level, opts)
-	opts = opts or {}
-	level = level or vim.log.levels.INFO
+--- Send a notification
+--- Title is automatically set to "foundry" when backend supports it
+--- @param msg string
+--- @param opts? NotifyOpts
+--- @return integer id Notification handle (for persistent notifications)
+function M.notify(msg, opts) end
 
-	vim.notify(msg, level)
-end
+--- Update a persistent notification message
+--- @param id integer
+--- @param msg string
+function M.update(id, msg) end
+
+--- Dismiss a notification
+--- @param id integer
+function M.dismiss(id) end
 
 return M
