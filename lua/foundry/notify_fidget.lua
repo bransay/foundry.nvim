866799b5487f9503871145471d52ee8d8c116616
Bryan Yeo <bryandismas@simiansoftwerks.com>
2026-03-01 12:07:06 -0800

    implement notify

--- lua/foundry/notify_fidget.lua
+++ lua/foundry/notify_fidget.lua
@@ -1,11 +1,30 @@
-local NOTIFICATION_TITLE = "foundry"
+local NOTIFICATION_TITLE = "Foundry"
+
+local fidget_notify = require("fidget.notification")
 
 local M = {}
+
+local next_id = 0
 
 --- @param msg string
 --- @param opts? NotifyOpts
 --- @return integer id
-function M.notify(msg, opts) end
+function M.notify(msg, opts)
+	opts = opts or {}
+	local level = opts.level or vim.log.levels.INFO
+
+	next_id = next_id + 1
+
+	local fidget_opts = {
+		annote = NOTIFICATION_TITLE,
+		ttl = opts.keep and math.huge or nil,
+		key = next_id,
+	}
+
+	fidget_notify.notify(msg, level, fidget_opts)
+
+	return next_id
+end
 
 --- @param id integer
 --- @param msg string
