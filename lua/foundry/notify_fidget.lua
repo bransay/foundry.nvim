daf109e2912286a3ea762f2a95d6f0c8459aec58
Bryan Yeo <bryandismas@simiansoftwerks.com>
2026-03-01 12:46:41 -0800

    implementation for fidget integration complete.

--- lua/foundry/notify_fidget.lua
+++ lua/foundry/notify_fidget.lua
@@ -2,6 +2,7 @@ local NOTIFICATION_TITLE = "Foundry"
 local NOTIFICATION_GROUP = "foundry"
 
 local fidget_notify = require("fidget.notification")
+local fidget_spinner = require("fidget.spinner")
 
 -- Register our notification group with a custom name
 fidget_notify.set_config(NOTIFICATION_GROUP, {
@@ -14,52 +15,71 @@ local M = {}
 local next_id = 0
 local active_notifications = {}
 
+-- Create spinner animation for persistent notifications (1 second cycle)
+local spinner_anime = fidget_spinner.animate("dots_negative", 1)
+
 --- @param msg string
 --- @param opts? NotifyOpts
---- @return integer id
+--- @return integer? id Notification handle (only for persistent notifications)
 function M.notify(msg, opts)
 	opts = opts or {}
 	local level = opts.level or vim.log.levels.INFO
 
+	if not opts.keep then
+		fidget_notify.notify(msg, level, {
+			group = NOTIFICATION_GROUP,
+		})
+		return nil
+	end
+
 	next_id = next_id + 1
 	local id = next_id
 
-	active_notifications[id] = level
-
-	local fidget_opts = {
-		annote = NOTIFICATION_TITLE,
-		group = NOTIFICATION_GROUP,
-		key = id,
+	active_notifications[id] = {
+		level = level,
+		msg = msg,
 	}
 
-	if opts.keep then
-		fidget_opts.ttl = math.huge
-	end
-
-	fidget_notify.notify(msg, level, fidget_opts)
+	fidget_notify.notify(msg, level, {
+		group = NOTIFICATION_GROUP,
+		key = id,
+		ttl = math.huge,
+	})
 
 	return id
 end
 
---- @param id integer
+--- @param id integer?
 --- @param msg string
 function M.update(id, msg)
-	local level = active_notifications[id] or vim.log.levels.INFO
+	if not id then
+		return
+	end
 
-	local fidget_opts = {
-		annote = NOTIFICATION_TITLE,
+	local notif = active_notifications[id]
+	if not notif then
+		return
+	end
+
+	-- Prepend spinner frame to message
+	local spinner_frame = spinner_anime(vim.loop.now() / 1000)
+	local full_msg = string.format("%s %s", spinner_frame, msg)
+
+	fidget_notify.notify(full_msg, notif.level, {
 		group = NOTIFICATION_GROUP,
 		key = id,
 		update_only = true,
-	}
-
-	fidget_notify.notify(msg, level, fidget_opts)
+		ttl = math.huge,
+	})
 end
 
---- @param id integer
+--- @param id integer?
 function M.dismiss(id)
-	active_notifications[id] = nil
+	if not id then
+		return
+	end
 
+	active_notifications[id] = nil
 	fidget_notify.remove(NOTIFICATION_GROUP, id)
 end
 
