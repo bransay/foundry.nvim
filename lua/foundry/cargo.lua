f65ccca997bee73e2c7535850282819c87387b04
Bryan Yeo <bryeo@YeoDesktop.lan>
2026-03-20 11:33:03 -0700

    added options stubs for build

--- lua/foundry/cargo.lua
+++ lua/foundry/cargo.lua
@@ -1,9 +1,38 @@
 local M = {}
 local foundry_notify = require('foundry.notify')
+local foundry_options = require('foundry.options')
 
 function M.detect(root)
 	local cargo_file = vim.fs.joinpath(root, 'Cargo.toml')
 	return vim.fn.filereadable(cargo_file) == 1
+end
+
+local function get_profile_options()
+	return {}
+end
+
+local function get_target_options()
+	return {}
+end
+
+local options = {
+	PROFILE = { 'profile', 'Profile', foundry_options.select_picker(get_profile_options) },
+	TARGET = { 'target', 'Target', foundry_options.select_picker(get_target_options) },
+}
+
+local function get_option(option, default, show_ui)
+	show_ui = show_ui or false
+	local opts = {}
+	if show_ui then
+		opts.force_ui = true
+	end
+	if option[3] then
+		opts.picker = option[3]
+	end
+	if default then
+		opts.default = default
+	end
+	return foundry_options.get(option[1], option[2], opts)
 end
 
 function M.build()
