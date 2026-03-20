269b6c4aaff8f916d7b7e31fd273269a9a4840da
Bryan Yeo <bryeo@YeoDesktop.lan>
2026-03-20 11:26:43 -0700

    add stub for cargo

--- lua/foundry/cargo.lua
+++ lua/foundry/cargo.lua
@@ -1,12 +1,19 @@
 local M = {}
+local foundry_notify = require('foundry.notify')
 
 function M.detect(root)
 	local cargo_file = vim.fs.joinpath(root, 'Cargo.toml')
 	return vim.fn.filereadable(cargo_file) == 1
 end
 
+function M.build()
+	foundry_notify.notify('Build triggered', { level = vim.log.levels.INFO })
+end
+
 function M.actions()
-	return {}
+	return {
+		{ name = 'Build', action = M.build },
+	}
 end
 
 return M
